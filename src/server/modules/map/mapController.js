1e956d0101bfd24f35e7379ea975e924ea1508dd
macklevine <mackplevine@gmail.com>
2015-09-06 12:58:02 -0700

    made some more tweaks

--- src/server/modules/map/mapController.js
+++ src/server/modules/map/mapController.js
@@ -1,8 +1,30 @@
 var sendResp = require('../../config/helpers').sendResponse,
-    dB       = require('../../db/db.js');
+    db       = require('../../db/db.js');
+
+var Event    = db.Event,
+  User     = db.User,
+  Map    = db.Map;
 
 module.exports = {
-  getMap: function(event){
+  getMap: function(eventCode){
+    Event.findOne({eventCode: eventCode}, function(err, event){
+      //TODO: handle error
+      if (!event){
+        console.log("error: event not found");
+      } else if (event){
+        Map.findOne({_parentEvent: event._id}, function(err, map){
+          //TODO: handle error
+          if (!map){
+            console.log("error: map not found");
+          } else if (map){
+            
+          }
+        })
+      }
+    });
+  },
+
+  saveMap: function(event, data){
     
   }
 };
