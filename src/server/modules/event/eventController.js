e748822bcb20ec3e9389a9aeb9ce01c1e0a96212
macklevine <mackplevine@gmail.com>
2015-09-08 21:46:10 -0700

    did some work so that the event controller invokes the event instantation service using the correct arguments.

--- src/server/modules/event/eventController.js
+++ src/server/modules/event/eventController.js
@@ -20,21 +20,26 @@ module.exports = {
   },
 
   createEvent: function (req, res){
-    // console.log(req);
-    console.log(req.file);
-    console.log(req.file.path);
-    var eventCode = req.body.eventCode;
+
+    // for debugging
+    // console.log(req.file);
+    // console.log(req.file.path);
+
+    var eventCode = req.body.eventCode,
+        eventName = req.body.eventName,
+        facebookId = req.body.facebookId;
+
     console.log(eventCode + " is our event code");
     dB.Event.findOne({eventCode: eventCode}, function(err, event){
       if (err){
         console.log(err);
       }
       if (event){
-        sendResp(res, {event: false});
+        sendResp(res, {event: "sorry, that event code already exists"});
       } else {
         cloudinary.postImages(req, res, function(result){
           console.log(result.url + " is the result we got back!");
-          mapmaker.saveEventAndMap("mack", result.url, eventCode, function(returnObj){
+          mapmaker.saveEventAndMap(facebookId, result.url, eventCode, eventName, function(returnObj){
             res.json(returnObj);
             sendResp(res);
           });
